5c157983573fc5bbae8373160cef55b8ce5ceadc
Giovani Bettoni <bettoni.gn@gmail.com>
2019-10-21 01:23:39 -0300

    Curso Spring Boot aula 04: Salvando dados do formulário no banco de dados

--- eventoapp/src/main/java/com/eventoapp/eventoapp/models/Evento.java
+++ eventoapp/src/main/java/com/eventoapp/eventoapp/models/Evento.java
@@ -1,12 +1,24 @@
 package com.eventoapp.eventoapp.models;
 
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
-public class Evento {
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Entity
+public class Evento implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+	private long codigo;
 	
 	private String nome;
 	private String local;
